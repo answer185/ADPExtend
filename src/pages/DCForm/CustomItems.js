526708b099a1fd60b0ee876163a93e43477c4d36
曾咏杰 <dc@cengyongjiedeMacBook-Pro.local>
2018-09-10 16:00:10 +0800

    inital commit

---
+++ src/pages/DCForm/CustomItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CustomItems extends Component {
+  render() {
+    return (
+      <div>开发中...</div>
+    );
+  }
+}
+
+CustomItems.propTypes = {};
+
+export default CustomItems;
